88a7afeb3a297e91574a4759c7e4d938918a3392
hardik-erda <hardik.erda27@gmail.com>
2022-09-28 10:37:02 +0530

    Empty activity added based on wireFrame

---
+++ app/src/main/java/com/example/freelancerjobportltsee/ProjectBidersListPage.java
@@ -0,0 +1,14 @@
+package com.example.freelancerjobportltsee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProjectBidersListPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_biders_list_page);
+    }
+}
