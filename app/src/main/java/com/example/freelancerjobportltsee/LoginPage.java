201d06fd4f036ade2783007315ed191a484f4e95
hardik-erda <hardik.erda27@gmail.com>
2022-10-12 11:04:35 +0530

    activities Created based on wireFrame.

--- app/src/main/java/com/example/freelancerjobportltsee/LoginPage.java
+++ app/src/main/java/com/example/freelancerjobportltsee/LoginPage.java
@@ -3,6 +3,9 @@ package com.example.freelancerjobportltsee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        TextView tv_forget = findViewById(R.id.tv_forget);
+        tv_forget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(LoginPage.this, "forget", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
