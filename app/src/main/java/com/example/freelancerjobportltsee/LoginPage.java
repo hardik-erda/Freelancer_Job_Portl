4d7b410215b79231e7567a5dcfda600c62a71827
hardik-erda <hardik.erda27@gmail.com>
2022-11-03 11:35:05 +0530

    register and login user with firebase auth(test).

--- app/src/main/java/com/example/freelancerjobportltsee/LoginPage.java
+++ app/src/main/java/com/example/freelancerjobportltsee/LoginPage.java
@@ -1,16 +1,32 @@
 package com.example.freelancerjobportltsee;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class LoginPage extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
+public class LoginPage extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    EditText edt_user,edt_password;
+    ProgressBar progressbar;
+    TextView tv_reg;
+    Button btn_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
         TextView tv_forget = findViewById(R.id.tv_forget);
@@ -20,5 +36,100 @@ public class LoginPage extends AppCompatActivity {
                 Toast.makeText(LoginPage.this, "forget", Toast.LENGTH_SHORT).show();
             }
         });
+        //firebase auth added
+        mAuth = FirebaseAuth.getInstance();
+        //initialising username and password
+        edt_user = findViewById(R.id.edt_user);
+        edt_password = findViewById(R.id.edt_password);
+        btn_login = findViewById(R.id.btn_login);
+        progressbar = findViewById(R.id.progressBar);
+        tv_reg = findViewById(R.id.tv_reg);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginUserAccount();
+            }
+        });
+
+        tv_reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginPage.this,RegisterPage.class);
+                startActivity(i);
+            }
+        });
+    }
+    private void loginUserAccount()
+    {
+
+        // show the visibility of progress bar to show loading
+        progressbar.setVisibility(View.VISIBLE);
+
+        // Take the value of two edit texts in Strings
+        String email, password;
+        email = edt_user.getText().toString();
+        password = edt_password.getText().toString();
+
+        // validations for input email and password
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(),
+                            "Please enter email!!",
+                            Toast.LENGTH_LONG)
+                    .show();
+            progressbar.setVisibility(View.GONE);
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(),
+                            "Please enter password!!",
+                            Toast.LENGTH_LONG)
+                    .show();
+            progressbar.setVisibility(View.GONE);
+            return;
+        }
+
+        // signin existing user
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                        new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(
+                                    @NonNull Task<AuthResult> task)
+                            {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getApplicationContext(),
+                                                    "Login successful!!",
+                                                    Toast.LENGTH_LONG)
+                                            .show();
+
+                                    // hide the progress bar
+                                    progressbar.setVisibility(View.GONE);
+
+                                    // if sign-in is successful
+                                    // intent to home activity
+                                    Intent intent
+                                            = new Intent(LoginPage.this,
+                                            MainActivity.class);
+                                    //send uid to mainActivity
+                                    intent.putExtra("user",mAuth.getUid());
+                                    startActivity(intent);
+
+                                }
+
+                                else {
+
+                                    // sign-in failed
+                                    Toast.makeText(getApplicationContext(),
+                                                    "Login failed!!",
+                                                    Toast.LENGTH_LONG)
+                                            .show();
+
+                                    // hide the progress bar
+                                    progressbar.setVisibility(View.GONE);
+                                }
+                            }
+                        });
     }
 }
--- app/src/main/java/com/example/freelancerjobportltsee/MainActivity.java
+++ app/src/main/java/com/example/freelancerjobportltsee/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.freelancerjobportltsee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent i = getIntent();
+        String val = i.getStringExtra("user");
+        TextView tv = findViewById(R.id.tv);
+        tv.setText(val);
     }
 }
